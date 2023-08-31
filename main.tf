4d0c8b1b913ade1f665eaee8133b0c57dcdb9070
Rick Ortega <rick.d.ortega@gmail.com>
2023-08-31 15:28:02 -0700

    added basic modules

---
+++ main.tf
@@ -0,0 +1,10 @@
+# snowflake
+
+terraform {
+    required_providers {
+        snowflake = {
+            source = "hashicorp/aws"
+            version = "~> 4.44.0"
+        }
+    }
+}
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "snowflake" {
+    account = ""
+    region = ""
+    username = ""
+    role = ""
+    password = ""
+}
+
